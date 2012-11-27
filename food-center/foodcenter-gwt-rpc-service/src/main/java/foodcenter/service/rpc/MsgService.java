a22f0ceaf07b0727921576a6aa5844db315e5c2c
dror.baum <dror.baum@af7f8a85-af02-1ce6-eb6c-32e1031288a0>
2012-11-27 23:39:33 +0000

    - mavenized!  - created modules + eclipse integration  - added docs + readme.txt

---
+++ food-center/foodcenter-gwt-rpc-service/src/main/java/foodcenter/service/rpc/MsgService.java
@@ -0,0 +1,19 @@
+package foodcenter.service.rpc;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("msgs")
+public interface MsgService extends RemoteService
+{
+	public List<String> getMsgs() throws IllegalArgumentException;
+	
+	public void addMsg(String msg) throws IllegalArgumentException;
+	
+	public void removeMsg(String msg) throws IllegalArgumentException;
+}
