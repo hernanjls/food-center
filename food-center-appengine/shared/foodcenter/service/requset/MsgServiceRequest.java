55b1962af12610022f9acdac937a9a8f4e072da3
dror.baum <dror.baum@af7f8a85-af02-1ce6-eb6c-32e1031288a0>
2013-09-05 21:14:16 +0000

    fix compilation issue with deprected MsgServiceRequest

--- food-center-appengine/shared/foodcenter/service/requset/MsgServiceRequest.java
+++ food-center-appengine/shared/foodcenter/service/requset/MsgServiceRequest.java
@@ -1,21 +1,16 @@
 package foodcenter.service.requset;
 
-import java.util.List;
-
-import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
 import com.google.web.bindery.requestfactory.shared.ServiceName;
-
-import foodcenter.service.proxies.MsgProxy;
 
 @ServiceName(value="foodcenter.server.service.MsgService")
 public interface MsgServiceRequest extends RequestContext
 {
 
-	Request<Void> createMsg(String msg);
-
-	Request<Void> deleteMsg(String msg);
-
-	Request<List<MsgProxy>> getMsgs();
+//	Request<Void> createMsg(String msg);
+//
+//	Request<Void> deleteMsg(String msg);
+//
+//	Request<List<MsgProxy>> getMsgs();
 
 }
