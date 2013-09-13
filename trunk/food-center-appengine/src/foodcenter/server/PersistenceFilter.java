7e448bbd75821a6bad5f300da9e1ddffaade47ad
dror.baum <dror.baum@af7f8a85-af02-1ce6-eb6c-32e1031288a0>
2013-09-13 23:06:37 +0000

    formatting and cleanups

--- trunk/food-center-appengine/src/foodcenter/server/PersistenceFilter.java
+++ trunk/food-center-appengine/src/foodcenter/server/PersistenceFilter.java
@@ -2,7 +2,6 @@ package foodcenter.server;
 
 import java.io.IOException;
 
-import javax.jdo.Transaction;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
